7ac5597be8543a318b0aa9f3372f8f53dc79ba93
evil-A <57819409+evil-A@users.noreply.github.com>
2023-05-05 16:41:57 +0200

    Add files via upload

--- python/p01/ex02/test.py
+++ python/p01/ex02/test.py
@@ -1,9 +1,26 @@
+#!/usr/bin/env python3
+
 from vector import Vector
 
-v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
-v2 = Vector([[0.0, 1.0, 2.0, 3.0]])
+
+#v1 = Vector([[0.0], [1.0], [2.0], [3.0]]) ### (4, 1)
+#v2 = Vector([[0.0, 1.0, 2.0, 3.0]]) ### (1, 4)
+
+#print("vector shape ", v1.shape)
+#v3 = v1 * 5
+#v4 = v2 * 5
+#print(v3)
+#v4 = v2 * 5
 #print(type([[0.0], [1.0], [2.0], [3.0]]))
 #print(type([[0.0, 1.0, 2.0, 3.0]]))
+
+v5 = Vector(4)
+
+#print(v1.shape)
+#print(v2.shape)
+
+#print(Vector([[0.0], [1.0], [2.0], [3.0]]).shape)
+#print(Vector([[0.0, 1.0, 2.0, 3.0]]).shape)
 
 #print(v1)
 #print(type(v1))
--- python/p01/ex02/vector.py
+++ python/p01/ex02/vector.py
@@ -1,3 +1,6 @@
+from operator import mul
+from itertools import starmap
+
 class Vector:
     def __init__(self, values):
         if isinstance(values, list):
@@ -22,11 +25,32 @@ class Vector:
                     self.shape = (1, len(values[0]))
                 else:
                     print("ERROR: bad input")
-            print(self.values)###
-            print(self.shape)###
+        elif isinstance(values, int):
+            self.shape = (1, values)
+            self.values = [[*range(0, values)]]
+            i = 0
+            for elem in self.values[0]:
+                self.values[0][i] *= 1.0
+                i += 1
+            print("(1, X)\nshape ", self.shape, "\nvalues ", self.values)
 
     def __str__(self):
         return "Vector({})".format(str(self.values))
+
+    def __mul__(self, other):
+        if type(other) ==  int or type(other) == float:
+            aux = self
+            i = 0
+            if self.shape[0] == 1: ### (1, X)
+                for elem in self.values[0]:
+                    aux.values[0][i] = aux.values[0][i] * other
+                    i += 1
+                print("(1, X)\nshape ", aux.shape, "\nvalues ", aux.values)
+            elif self.shape[1] == 1: ### (X, 1)
+                for elem in self.values:
+                    aux.values[i][0] = aux.values[i][0] * other
+                    i += 1
+                print("(X, 1)\nshape ", aux.shape, "\nvalues ", self.values)
 
 
 """
